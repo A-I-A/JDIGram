e0f018c9d278474b2acd5ad902f24293d7730eb7
A-I-A <box112015@gmail.com>
2021-05-24 16:42:33 +0300

    profile editing is now available only for its owner

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!, only: [:index, :edit, :update, :destroy]
-  before_action :find_user, only: [:show]
+  before_action :find_user, only: [:show, :edit]
+  before_action :permit_only_current_user, only: [:edit, :destroy]
 
   def index
     @users = User.all
@@ -24,6 +25,10 @@ class UsersController < ApplicationController
   def find_user
     @user = User.find_by(id: params[:id])
     render_404 unless @user
+  end
+
+  def permit_only_current_user
+    render_403 unless @user.id == current_user.id
   end
 
 end
