4b6368edc78ea392198c6c29c692854f8d599835
A-I-A <box112015@gmail.com>
2021-05-24 15:36:59 +0300

    first attempt to add users profie and users index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+   users_path
+  end
+
   protected
-    def configure_permitted_parameters
+
+  def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys:[:login, :name])
-    end
+  end
+  
+  private
+  
+  def render_404
+    render file: "#{Rails.root}/public/404.html", status: 404
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index, :edit, :update, :destroy]
+  before_action :find_user, only: [:show]
 
-  def profile
+  def index
+    @users = User.all
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def udate
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by(id: params[:id])
+    render_404 unless @user
+  end
+
 end
