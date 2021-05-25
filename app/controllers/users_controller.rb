5b05a7f5afcd04b4dd7f20bf4e48375b901445cc
A-I-A <box112015@gmail.com>
2021-05-25 17:19:53 +0300

    first sketch of navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
   before_action :find_user, only: [:show, :edit, :update, :destroy]
   before_action :permit_only_current_user, only: [:edit, :destroy]
 
+  layout "users"
+
   def index
     @users = User.all
   end
