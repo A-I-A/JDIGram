b5d20244a75ebfc8135c88d5828b998465565f30
A-I-A <box112015@gmail.com>
2021-06-09 09:02:31 +0300

    in avatar.js installation / removal of a spinner for avatars has been moved to special functions

--- app/assets/javascripts/avatar.js
+++ app/assets/javascripts/avatar.js
@@ -1,86 +1,92 @@
 $(document).on("turbolinks:load", function(){
   
   if ($('#cropModal').length){
-    var cropModal = new bootstrap.Modal($('#cropModal'));
-  }
+    let cropModal = new bootstrap.Modal($('#cropModal'));
 
-  $('#cropModal').on('hide.bs.modal', function(){ 
-    $('#crop-img').cropper('destroy');
-    $(".avatar-file-field").val(null);
-  })
-    
-  $('#cropModal').on('shown.bs.modal', function(){  
-    let reader = new FileReader();  
-    reader.onload = function(event){
-      $("#crop-img").attr('src', event.target.result);
-    }
-    reader.readAsDataURL($(".avatar-file-field")[0].files[0]);
-  })
-
-  $(".avatar-file-field").on('input', function(event){ 
-    cropModal.toggle();
-  })
-
-  $("#crop-img").on('load', function(){
-    $('#crop-img').cropper({
-      viewMode: 1,
-      movable: false,
-      rotatable: false,
-      zoomable: false,
-      preview: ".crop-preview",
-      dragMode: 'none',
-      guides: false,
-      aspectRatio: 1 / 1,
-      crop: function(event) {
-      }
+    $('#cropModal').on('hide.bs.modal', function(){ 
+      $('#crop-img').cropper('destroy');
+      $(".avatar-file-field").val(null);
     })
-  })
-    
-  $("#crop-button").click(function(event){
-    $(".edit-avatar").css('opacity','0.1');
-    $(".edit-avatar-spinner").css('display','block');
-    $(".show-avatar").css('opacity','0.1');
-    $(".show-avatar-spinner").css('display','block');
-    $(".navbar-avatar").css('opacity','0.1');
-    $(".navbar-avatar-spinner").css('display','block');
-    $('#crop-img').cropper('getCroppedCanvas', undefined).toBlob(function(blob){
-      let formData = new FormData();
-      formData.append('avatar', blob);
-      $.ajax({
-        url: "/users/set_avatar/" + event.target.getAttribute("user_id"),
-        type: 'POST',
-        processData: false,
-        contentType: false,
-        data: formData,
-        success: function(data){
-          $(".edit-avatar").attr('src', data.avatar_url);
-          $(".show-avatar").attr('src', data.avatar_url);
-          $(".navbar-avatar").attr('src', data.avatar_url);
-          cropModal.toggle();
-          Turbolinks.visit(window.location.toString(), { action: 'replace' });
-          flashOnAvatarChange("your avatar have been updated");
+
+    $('#cropModal').on('shown.bs.modal', function(){  
+      let reader = new FileReader();  
+      reader.onload = function(event){
+        $("#crop-img").attr('src', event.target.result);
+      }
+      reader.readAsDataURL($(".avatar-file-field")[0].files[0]);
+    })
+
+    $(".avatar-file-field").on('input', function(event){ 
+      cropModal.toggle();
+    })
+
+    $("#crop-img").on('load', function(){
+      $('#crop-img').cropper({
+        viewMode: 1,
+        movable: false,
+        rotatable: false,
+        zoomable: false,
+        preview: ".crop-preview",
+        dragMode: 'none',
+        guides: false,
+        aspectRatio: 1 / 1,
+        crop: function(event) {
         }
       })
     })
-  })
+
+    $("#crop-button").click(function(event){
+      setAvatarSpinner($(".edit-avatar"), $(".edit-avatar-spinner"));
+      setAvatarSpinner($(".show-avatar"), $(".show-avatar-spinner"));
+      setAvatarSpinner($(".navbar-avatar"), $(".navbar-avatar-spinner"));
+
+      $('#crop-img').cropper('getCroppedCanvas', undefined).toBlob(function(blob){
+        let formData = new FormData();
+        formData.append('avatar', blob);
+        $.ajax({
+          url: "/users/set_avatar/" + event.target.getAttribute("user_id"),
+          type: 'POST',
+          processData: false,
+          contentType: false,
+          data: formData,
+          success: function(data){
+            $(".edit-avatar").attr('src', data.avatar_url);
+            $(".show-avatar").attr('src', data.avatar_url);
+            $(".navbar-avatar").attr('src', data.avatar_url);
+            cropModal.toggle();
+            Turbolinks.visit(window.location.toString(), { action: 'replace' });
+            flashOnAvatarChange("your avatar have been updated");
+          }
+        })
+      })
+    })
+  }
+
 
   $(".edit-avatar").on('load', function(){
-    $(this).css('opacity','1');
-    $(".edit-avatar-spinner").css('display','none');
+    disableAvatarSpinner($(this), $(".edit-avatar-spinner"));
   })
 
   $(".show-avatar").on('load', function(){
-    $(this).css('opacity','1');
-    $(".show-avatar-spinner").css('display','none'); 
+    disableAvatarSpinner($(this), $(".show-avatar-spinner"));
   })
 
   $(".navbar-avatar").on('load', function(){
-    $(this).css('opacity','1');
-    $(".navbar-avatar-spinner").css('display','none'); 
+    disableAvatarSpinner($(this), $(".navbar-avatar-spinner")); 
   })
 
   function flashOnAvatarChange(str){
     $(".flash-info").html(str).fadeOut(3000);
+  }
+
+  function setAvatarSpinner(avatar, spinner){
+    avatar.css('opacity','0.1');
+    spinner.css('display','block');
+  }
+
+  function disableAvatarSpinner(avatar, spinner){
+    avatar.css('opacity','1');
+    spinner.css('display','none');
   }
 
   $(".remove_avatar-button").click(function(event){
@@ -90,7 +96,7 @@ $(document).on("turbolinks:load", function(){
       processData: false,
       contentType: false,
       success: function(){
-        Turbolinks.visit(window.location.toString(), { action: 'replace' });
+      Turbolinks.visit(window.location.toString(), { action: 'replace' });
       }
     })
   })
