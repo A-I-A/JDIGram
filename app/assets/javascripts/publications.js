9393dc64fd409b5856c9947e24d67e52c26527b7
Roman Oks <advenua@gmail.com>
2021-06-25 11:19:13 +0300

    Cleaned up stylesheets

--- app/assets/javascripts/publications.js
+++ app/assets/javascripts/publications.js
@@ -1,12 +1,12 @@
- $(document).on("turbolinks:load", function(){
+$(document).on("turbolinks:load", function() {
    let publicationPreview = $(".publication-preview");
    let userPublicationPreview = $(".user-publication-preview");
 
    publicationPreview.height(publicationPreview.width());
    userPublicationPreview.height(userPublicationPreview.width());
- 
+
    $(window).on("resize", function(){
-    publicationPreview.height(publicationPreview.width());
-    userPublicationPreview.height(userPublicationPreview.width());
-   })
-})
+       publicationPreview.height(publicationPreview.width());
+       userPublicationPreview.height(userPublicationPreview.width());
+   });
+});
