e6a96d567fe2a95ddc3b896e9b9fdd54e1c2586f
A-I-A <box112015@gmail.com>
2021-07-06 08:52:24 +0300

    little cleaning in publications controller spec file

--- spec/controllers/publication_controller_spec.rb
+++ spec/controllers/publication_controller_spec.rb
@@ -6,42 +6,36 @@ Rails.application.routes.default_url_options[:host] = :test
 
 RSpec.describe PublicationsController, type: :controller do
 
-  describe "when publication doesn't exist" do
+  describe "when user signed in" do
 
-    it "gives status 404" do
-     get :show, params: { id: -1 }
-     expect(response.status).to eq(404)
-    end
-
-    it "renders 404 page" do
-     get :show, params: { id: -1 }
-     expect(response).to render_template(file: "#{Rails.root}/public/404.html")
-    end
-
-  end
-
-  describe "when publication exists" do 
     let (:user) { create(:user) }
-    let (:file) { fixture_file_upload('cockroach.jpg', 'img/jpeg') }
-    let (:publication) { create(:publication, user_id: user.id, photos: file) }
+    before { sign_in user }
 
-    it "gives status 200" do
-      get :show, params: { id: publication.id}
-      expect(response.status).to eq(200)
+    describe "when publication doesn't exist" do
+
+      it "gives status 404" do
+       get :show, params: { id: -1 }
+       expect(response.status).to eq(404)
+      end
+
+      it "renders 404 page" do
+       get :show, params: { id: -1 }
+       expect(response).to render_template(file: "#{Rails.root}/public/404.html")
+      end
+
     end
 
-    it "sends test description" do 
-      get :show, params: { id: publication.id}
-      description = JSON.parse(response.body)["description"]
-      expect(description).to eq("test description")
+    describe "when publication exists" do 
+      let (:user1) { create(:user) }
+      before { sign_in user1 }
+      let (:file) { fixture_file_upload('cockroach.jpg', 'img/jpeg') }
+      let (:publication) { create(:publication, user_id: user1.id, photos: file) }
+
+      it "gives status 200" do
+        get :show, params: { id: publication.id }, xhr: true
+        expect(response.status).to eq(200)
+      end
     end 
-
-    it "keeps image file" do 
-      get :show, params: { id: publication.id}
-      photo_url = JSON.parse(response.body)['photos'][0]['photo_url']
-      expect(photo_url).to eq(rails_blob_path(publication.photos[0], disposition: "attachment"))
-    end
-    
   end
 
 end
